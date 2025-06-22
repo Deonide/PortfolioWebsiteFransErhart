8f7a38aac2e217a71d0ffa6b5b856320014f6884
FemBear <beer@vreeze.com>
2025-06-22 18:03:08 +0200

    changes to card

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -4,15 +4,16 @@ export default function Card({
   title, 
   description, 
   img, 
+  hreftitle, 
   href 
 }: { 
   title: string; 
   description: string; 
   img: string; 
+  hreftitle: string; 
   href: string; 
-}) {
-  return (
-    <Link href={href} className="group">
+}) {  return (
+    <Link href={href} className="group" title={hreftitle}>
       <div 
         className="relative shadow-lg rounded-2xl p-5 border border-gray-200 cardSize 
                    transition-transform transform hover:scale-105 hover:shadow-xl 
